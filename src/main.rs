b043709c59d5f83f426c698b406e92422f01096c
Chris Brown <xoebus@xoeb.us>
2016-10-30 12:55:58 -0700

    initial commit with simple credential detection

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::io::{self, BufReader, BufRead, Read};
+use std::fs::File;
+use std::path::Path;
+use std::process;
+
+extern crate ansi_term;
+
+use ansi_term::Colour::Red;
+
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::RegexSet;
+
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, App, AppSettings, SubCommand};
+
+fn main() {
+    let matches = App::new("axe")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about("manage credentials in log files")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name("detect")
+            .about("detect credentials in log files")
+            .arg(Arg::with_name("input")
+                .help("log file to scan")
+                .validator(is_file)
+                .index(1)))
+        .get_matches();
+
+
+    match matches.subcommand_name() {
+        Some("detect") => {
+            let detect_matches = matches.subcommand_matches("detect").unwrap();
+            let file = detect_matches.value_of("input");
+
+            let handle = match file {
+                Some(path) => Box::new(File::open(path).unwrap()) as Box<Read>,
+                None => Box::new(io::stdin()) as Box<Read>,
+            };
+
+            let buf = BufReader::new(handle);
+            let mut found = false;
+
+            for line in buf.lines()
+                .filter(|res| res.is_ok())
+                .map(|l| l.unwrap())
+                .filter(|l| line_contains_credential(&l)) {
+                found = true;
+                println!("[{}] {}", Red.bold().paint("CRED"), line);
+            }
+
+            if found {
+                process::exit(1);
+            }
+        }
+        _ => {}
+    }
+}
+
+fn line_contains_credential(line: &String) -> bool {
+    lazy_static! {
+        static ref RE: RegexSet = RegexSet::new(&[
+            r"(?i)password",
+            r"(?i)identified by",
+            r"(?i)key",
+            r"(?i)secret",
+        ]).unwrap();
+    }
+
+    RE.is_match(line)
+}
+
+fn is_file(val: String) -> Result<(), String> {
+	let path = Path::new(&val);
+
+	if path.exists() {
+		Ok(())
+	} else {
+		Err(format!("{:?} does not exist", path))
+	}
+}
