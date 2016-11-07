e492e89686506905960f407066235f4b79331d99
Chris Brown <xoebus@xoeb.us>
2016-11-06 20:25:07 -0800

    log lines can be encrypted

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::io::{self, BufReader, BufRead, Read};
-use std::fs::File;
-use std::path::Path;
+use std::io::{self, BufReader, BufRead};
 use std::process;
 
 extern crate ansi_term;
@@ -18,34 +16,39 @@ extern crate clap;
 
 use clap::{Arg, App, AppSettings, SubCommand};
 
+extern crate sodiumoxide;
+
+use sodiumoxide::crypto::secretbox::{self, Key};
+use sodiumoxide::crypto::hash;
+
+extern crate base64;
+
+use base64::encode;
+
 fn main() {
+    if !sodiumoxide::init() {
+        println!("failed to initialize crypto");
+        process::exit(1);
+    }
+
     let matches = App::new("axe")
         .author(crate_authors!())
         .version(crate_version!())
         .about("manage credentials in log files")
+        .setting(AppSettings::VersionlessSubcommands)
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(SubCommand::with_name("detect")
-            .about("detect credentials in log files")
-            .arg(Arg::with_name("input")
-                .help("log file to scan")
-                .validator(is_file)
-                .index(1)))
+        .subcommand(SubCommand::with_name("detect").about("detect credentials in log files"))
         .subcommand(SubCommand::with_name("filter")
             .about("filter credentials from log files")
-            .arg(Arg::with_name("filter")
-                .help("log file to filter")
-                .validator(is_file)
-                .index(1)))
+            .arg(Arg::with_name("encrypt")
+                .short("e")
+                .long("encryption-key")
+                .value_name("KEY")
+                .help("setting this value encrypts secret lines rather than redacting them")
+                .takes_value(true)))
         .get_matches();
 
-    let file = matches.value_of("input");
-
-    let handle = match file {
-        Some(path) => Box::new(File::open(path).unwrap()) as Box<Read>,
-        None => Box::new(io::stdin()) as Box<Read>,
-    };
-
-    let buf = BufReader::new(handle);
+    let buf = BufReader::new(io::stdin());
     let lines = buf.lines().filter(|res| res.is_ok()).map(|l| l.unwrap());
 
     match matches.subcommand_name() {
@@ -62,11 +65,34 @@ fn main() {
             }
         }
         Some("filter") => {
-            for line in lines {
-                if line_contains_credential(&line) {
-                    println!("+++ axe: Line contained a possible credential and has been removed.");
-                } else {
-                    println!("{}", line)
+            let encrypt = matches.subcommand_matches("filter").unwrap().value_of("encrypt");
+
+            match encrypt {
+                Some(key_flag) => {
+                    let extended_key = &hash::hash(key_flag.as_bytes())[..secretbox::KEYBYTES];
+                    let key = Key::from_slice(extended_key).unwrap();
+
+                    for line in lines {
+                        if line_contains_credential(&line) {
+                            let nonce = secretbox::gen_nonce();
+                            let ciphertext = secretbox::seal(line.as_bytes(), &nonce, &key);
+                            println!("+++ axe-encrypt: {} - {}",
+                                     encode(&nonce[..]),
+                                     encode(ciphertext.as_slice()));
+                        } else {
+                            println!("{}", line)
+                        }
+                    }
+                }
+                _ => {
+                    for line in lines {
+                        if line_contains_credential(&line) {
+                            println!("+++ axe: Line contained a possible credential and has been \
+                                      removed.");
+                        } else {
+                            println!("{}", line)
+                        }
+                    }
                 }
             }
         }
@@ -85,14 +111,4 @@ fn line_contains_credential(line: &String) -> bool {
     }
 
     RE.is_match(line)
-}
-
-fn is_file(val: String) -> Result<(), String> {
-    let path = Path::new(&val);
-
-    if path.exists() {
-        Ok(())
-    } else {
-        Err(format!("{:?} does not exist", path))
-    }
 }
