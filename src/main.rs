bd60ad593d0568de493fce6a6dd4c2468ba854fe
Chris Brown <xoebus@xoeb.us>
2016-11-06 20:54:32 -0800

    allow decryption of logs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufReader, BufRead};
 use std::process;
+use std::str;
 
 extern crate ansi_term;
 
@@ -18,12 +19,12 @@ use clap::{Arg, App, AppSettings, SubCommand};
 
 extern crate sodiumoxide;
 
-use sodiumoxide::crypto::secretbox::{self, Key};
+use sodiumoxide::crypto::secretbox::{self, Key, Nonce};
 use sodiumoxide::crypto::hash;
 
 extern crate base64;
 
-use base64::encode;
+use base64::{decode, encode};
 
 fn main() {
     if !sodiumoxide::init() {
@@ -40,11 +41,20 @@ fn main() {
         .subcommand(SubCommand::with_name("detect").about("detect credentials in log files"))
         .subcommand(SubCommand::with_name("filter")
             .about("filter credentials from log files")
-            .arg(Arg::with_name("encrypt")
-                .short("e")
+            .arg(Arg::with_name("key")
+                .short("k")
                 .long("encryption-key")
                 .value_name("KEY")
                 .help("setting this value encrypts secret lines rather than redacting them")
+                .takes_value(true)))
+        .subcommand(SubCommand::with_name("reconstruct")
+            .about("decrypt credentials from log files")
+            .arg(Arg::with_name("key")
+                .short("k")
+                .long("encryption-key")
+                .value_name("KEY")
+                .required(true)
+                .help("setting this value decrypts secret lines from the input")
                 .takes_value(true)))
         .get_matches();
 
@@ -65,7 +75,7 @@ fn main() {
             }
         }
         Some("filter") => {
-            let encrypt = matches.subcommand_matches("filter").unwrap().value_of("encrypt");
+            let encrypt = matches.subcommand_matches("filter").unwrap().value_of("key");
 
             match encrypt {
                 Some(key_flag) => {
@@ -79,6 +89,39 @@ fn main() {
                             println!("+++ axe-encrypt: {} - {}",
                                      encode(&nonce[..]),
                                      encode(ciphertext.as_slice()));
+                        } else {
+                            println!("{}", line)
+                        }
+                    }
+                }
+                _ => {
+                    for line in lines {
+                        if line_contains_credential(&line) {
+                            println!("+++ axe: Line contained a possible credential and has been \
+                                      removed.");
+                        } else {
+                            println!("{}", line)
+                        }
+                    }
+                }
+            }
+        }
+        Some("reconstruct") => {
+            let encrypt = matches.subcommand_matches("reconstruct").unwrap().value_of("key");
+
+            match encrypt {
+                Some(key_flag) => {
+                    let extended_key = &hash::hash(key_flag.as_bytes())[..secretbox::KEYBYTES];
+                    let key = Key::from_slice(extended_key).unwrap();
+
+                    for line in lines {
+                        if line.starts_with("+++ axe-encrypt:") {
+                            let tokens = line.split(" ").collect::<Vec<_>>();
+                            let nonce = Nonce::from_slice(&decode(tokens[2]).unwrap()).unwrap();
+                            let ciphertext = decode(tokens[4]).unwrap();
+                            let plaintext = secretbox::open(&ciphertext, &nonce, &key).unwrap();
+
+                            println!("{}", str::from_utf8(&plaintext).unwrap())
                         } else {
                             println!("{}", line)
                         }
